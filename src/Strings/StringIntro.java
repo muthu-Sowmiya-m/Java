3522febbd726a5db5bfa4a1739c9055b848b3745
Muthu Sowmiya M <muthusowmya218@gmail.com>
2022-03-21 08:35:59 +0530

    18/7/22

--- src/Strings/StringIntro.java
+++ src/Strings/StringIntro.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 import java.lang.*;
 //import java.util.*;
 public class StringIntro {
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         Scanner sc = new Scanner(System.in);
-        //normal string declaration
+
+        //normal string declarations
         String s1 = "Hello there";
-        //string declaration using stringb class
+
+        //string declaration using string class
         String ob = new String("hello there");
+
         char arr[] = {'h', 'e', 'l', 'l', 'o'};
         //Converting array to string
         String ob2 = new String(arr);
