d1dae684439b82201ccc04b01349f273a5f7ef45
Muthu Sowmiya M <muthusowmya218@gmail.com>
2022-03-21 10:14:24 +0530

    18/7/22

--- src/OOPS /Inheritance.java
+++ src/OOPS /Inheritance.java
@@ -1,15 +1,69 @@
 import java.lang.*;
-class C
+
+//Hierarchical inheritance
+class Animal
 {
-    int a = 20;
+    void sound()
+    {
+        System.out.println("$$$$$$$");
+    }
 }
+
 //Single Inheritance
-public class Inheritance extends C
+class Dog extends Animal
+{
+    @Override
+    void sound()
+    {
+        //Using super to call paarent class method
+        super.sound();
+        System.out.println("bark!!");
+    }
+}
+
+class Cat extends Animal
+{
+    @Override
+    void sound()
+    {
+        super.sound();
+        System.out.println("meow!!");
+    }
+}
+
+public class Inheritance extends Cat
 {
     public static void main(String args[])
     {
-        Inheritance ob = new Inheritance();
-        System.out.println(ob.a);
+        Cat obj = new Cat();
+        obj.sound();
+
+        Inheritance obj2 = new Inheritance();
+        obj2.sound();
+
+        Dog obj3 = new Dog();
+        obj3.sound();
     }
 }
+
+/*Multilevel Inheritance
+class Dog extends Animal
+{
+    @Override
+    void sound()
+    {
+        super.sound();
+        System.out.println("bark!!");
+    }
+}
+class BabyDog extends Dog
+{
+    @Override
+    void sound()
+    {
+        super.sound();
+        System.out.println("Whine!!");
+    }
+}
+ */
 
